21088f5149bd45997f4874ffa0d517f9271ead8b
Perttu Ahola <celeron55@gmail.com>
2014-10-27 14:25:38 +0200

    Revert "CMake option: URHO3D_ENFORCE_SINGLE_THREAD (default TRUE)"
    This reverts commit 697c52df67d1a56f7a553df865703688b55e9899.
    
    Not needed anymore.

--- Source/Engine/Core/Object.cpp
+++ Source/Engine/Core/Object.cpp
@@ -224,13 +224,11 @@ void Object::SendEvent(StringHash eventType)
 
 void Object::SendEvent(StringHash eventType, VariantMap& eventData)
 {
-#ifdef URHO3D_ENFORCE_SINGLE_THREAD
     if (!Thread::IsMainThread())
     {
         LOGERROR("Sending events is only supported from the main thread");
         return;
     }
-#endif
     
     // Make a weak pointer to self to check for destruction during event handling
     WeakPtr<Object> self(this);
